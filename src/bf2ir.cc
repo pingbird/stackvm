983007ecbb948ed4c35b81e9d3a10acbd9eb76a1
Andre Lipke <andre.lipke@gmail.com>
2020-08-24 18:16:32 -0400

    More asserts

--- src/bf2ir.cc
+++
@@ -1,2 +0,0 @@
-#include "bf2ir.h"
-
--- src/opt/validate.cc
+++ src/opt/validate.cc
@@ -7,16 +7,20 @@
 using namespace IR;
 
 void Opt::validate(Graph *graph) {
-  std::unordered_set<int> ids;
+#ifdef NDEBUG
+  return;
+#else
+  std::unordered_set<int> instIds;
+
   for (Block *block : graph->blocks) {
     assert(block->id < graph->nextBlockId);
     std::vector<Inst*> insts;
     Inst *cur = block->first;
     while (cur != nullptr) {
       insts.push_back(cur);
-      assert(!ids.count(cur->id));
+      assert(!instIds.count(cur->id));
       assert(cur->id < graph->nextInstId);
-      ids.insert(cur->id);
+      instIds.insert(cur->id);
       cur = cur->next;
     }
     assert(insts[insts.size() - 1] == block->last);
@@ -27,20 +31,37 @@ void Opt::validate(Graph *graph) {
     }
   }
 
+  bool first = true;
   for (Block *block : graph->blocks) {
+    if (first) {
+      assert(block->predecessors.empty());
+      first = false;
+    }
+
+    for (Block *predecessor : block->predecessors) {
+      auto &v = predecessor->successors;
+      assert(std::find(v.begin(), v.end(), block) != v.end());
+    }
+
+    for (Block *successor : block->successors) {
+      auto &v = successor->predecessors;
+      assert(std::find(v.begin(), v.end(), block) != v.end());
+    }
+
     Inst *cur = block->first;
     while (cur != nullptr) {
       for (Inst *input : cur->inputs) {
-        assert(ids.count(input->id));
+        assert(instIds.count(input->id));
         auto &v = input->outputs;
         assert(std::find(v.begin(), v.end(), cur) != v.end());
       }
       for (Inst *output : cur->outputs) {
-        assert(ids.count(output->id));
+        assert(instIds.count(output->id));
         auto &v = output->inputs;
         assert(std::find(v.begin(), v.end(), cur) != v.end());
       }
       cur = cur->next;
     }
   }
+#endif
 }
