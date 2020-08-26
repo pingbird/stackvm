9a033729371f237d11161c4b8c9e36b108dd7f27
Andre Lipke <andre.lipke@gmail.com>
2020-08-26 19:39:05 -0400

    More instruction tests

--- src/opt/validate.cc
+++ src/opt/validate.cc
@@ -55,11 +55,55 @@ void Opt::validate(Graph *graph) {
         auto &v = input->outputs;
         assert(std::find(v.begin(), v.end(), cur) != v.end());
       }
+
       for (Inst *output : cur->outputs) {
         assert(instIds.count(output->id));
         auto &v = output->inputs;
         assert(std::find(v.begin(), v.end(), cur) != v.end());
       }
+
+      assert(cur->block == block);
+      assert(cur->mounted);
+      switch (cur->kind) {
+        case I_NOP:
+          assert(cur->inputs.empty());
+          assert(cur->outputs.empty());
+          break;
+        case I_REG:
+        case I_IMM:
+        case I_GETCHAR:
+          assert(cur->inputs.empty());
+          break;
+        case I_SUB:
+        case I_ADD:
+        case I_STR:
+          assert(cur->inputs.size() == 2);
+          break;
+        case I_SETREG:
+        case I_LD:
+        case I_PUTCHAR:
+          assert(cur->inputs.size() == 1);
+          break;
+        case I_PHI:
+          assert(cur->inputs.size() == block->predecessors.size());
+          assert(cur->next == nullptr);
+          break;
+        case I_IF:
+          assert(cur->inputs.size() == 2);
+          assert(cur->next == nullptr);
+          break;
+        case I_GOTO:
+          assert(cur->inputs.empty());
+          assert(cur->next == nullptr);
+          assert(block->predecessors.size() == 1);
+          break;
+        case I_RET:
+          assert(cur->inputs.empty());
+          assert(cur->next == nullptr);
+          assert(block->predecessors.empty());
+          break;
+      }
+
       cur = cur->next;
     }
   }
