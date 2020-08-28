78c80617a61a1e62462b1a7d88f3b94efcd2900a
Andre Lipke <andre.lipke@gmail.com>
2020-08-27 22:22:56 -0400

    Fix clz implementation on gcc

--- src/common.h
+++ src/common.h
@@ -4,14 +4,14 @@
 
 namespace Math {
 #ifdef __GNUC__
-  static unsigned int ctz(uint32_t x) {
-    return __builtin_ctz(x);
+  static unsigned int clz(uint32_t x) {
+    return __builtin_clz(x);
   }
 #else
   static unsigned int clz(uint32_t x) {
     x = ~x;
     uint32_t t = 0;
-    while ((x >> (31 - t)) & 1) t++;
+    while ((x >> (31 - t)) & 1u) t++;
     return t;
   }
 #endif
