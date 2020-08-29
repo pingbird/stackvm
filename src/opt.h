1d15c86c8f7b2b27ed2b44930dce31ef7408de79
Andre Lipke <andre.lipke@gmail.com>
2020-08-28 23:36:18 -0400

    Start implementing fold optimization

--- src/opt.h
+++ src/opt.h
@@ -5,6 +5,12 @@
 namespace Opt {
   void resolveRegs(IR::Graph *graph);
   void validate(IR::Graph *graph);
-  void fold(IR::Graph *graph);
+
+  namespace Fold {
+    struct FoldRules;
+  }
+  Fold::FoldRules *defaultFoldRules();
+  void fold(Fold::FoldRules *rules, IR::Inst *inst);
+
   IR::TypeId resolveType(IR::Inst *inst);
 }
--- src/opt/fold.cc
+++ src/opt/fold.cc
@@ -1,7 +1,37 @@
 #include "fold.h"
 
 using namespace IR;
+using namespace Opt;
+using namespace Opt::Fold;
 
-void Opt::fold(Graph *graph) {
-  
+FoldRules *Opt::defaultFoldRules() {
+  auto rules = new FoldRules();
+
+  return rules;
+}
+
+void Opt::fold(FoldRules *rules, Inst *inst) {
+  switch (inst->kind) {
+    case I_NOP:
+    case I_IMM:
+    case I_LD:
+    case I_STR:
+    case I_REG:
+    case I_SETREG:
+    case I_GETCHAR:
+    case I_PUTCHAR:
+    case I_PHI:
+    case I_IF:
+    case I_GOTO:
+    case I_RET:
+      return;
+    case I_ADD:
+    case I_SUB:
+      break;
+  }
+
+  Inst *left = inst->inputs[0];
+  Inst *right = inst->inputs[1];
+
+  FoldFn folder = nullptr;
 }
